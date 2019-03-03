d61ea65e69e503a2008013b83e6947f1d8a2179f
mrgaturus <mrgaturus@gmail.com>
2019-03-03 12:32:08 -0500

    Binding: add change indicator

--- src/binding.rs
+++ src/binding.rs
@@ -1,21 +1,19 @@
 use std::marker::PhantomData;
 // TODO: recreate binding
+static mut CHANGED: bool = false;
 
-pub trait Binding<T> {
-    fn proxy(&self) -> BindProxy<T>;
-}
-
-pub struct PointerBinding<'a, T: 'a> {
-    ptr: *const T,
-    phantom: PhantomData<&'a T>
-}
-
-pub struct BoxBinding<T> {
-    data: Box<T>
+#[inline(always)]
+pub fn changed() -> bool {
+    unsafe {
+        let prev: bool = CHANGED;
+        CHANGED = false;
+        prev
+    }
 }
 
 pub struct BindProxy<T> {
-    ptr: *const T
+    ptr: *const T,
+    indicator: bool
 }
 
 impl <T> BindProxy<T> {
@@ -28,12 +26,28 @@ impl <T> BindProxy<T> {
     pub fn act_mut<F>(&self, func: F) where F: Fn(&mut T) {
         unsafe {
             func(&mut *( self.ptr as *mut T ));
+            if self.indicator {
+                CHANGED = true;
+            }
         }
     }
 }
 
+pub trait Binding<T> {
+    fn proxy(&self, indicator: bool) -> BindProxy<T>;
+}
+
+pub struct PointerBinding<'a, T: 'a> {
+    ptr: *const T,
+    phantom: PhantomData<&'a T>
+}
+
+pub struct BoxBinding<T> {
+    data: Box<T>
+}
+
 impl <'a, T> PointerBinding<'a, T> {
-    pub fn new(ptr: &'a T) -> Self {
+    pub fn new(ptr: &'a mut T) -> Self {
         PointerBinding {
             ptr: ptr as *const T,
             phantom: PhantomData
@@ -42,9 +56,10 @@ impl <'a, T> PointerBinding<'a, T> {
 }
 
 impl <'a, T> Binding<T> for PointerBinding<'a, T> {
-    fn proxy(&self) -> BindProxy<T> {
+    fn proxy(&self, indicator: bool) -> BindProxy<T> {
         BindProxy {
-            ptr: self.ptr as *const T
+            ptr: self.ptr as *const T,
+            indicator
         }
     }
 }
@@ -58,9 +73,10 @@ impl <T> BoxBinding<T> {
 }
 
 impl <T> Binding<T> for BoxBinding<T> {
-    fn proxy(&self) -> BindProxy<T> {
+    fn proxy(&self, indicator: bool) -> BindProxy<T> {
         BindProxy {
-            ptr: self.data.as_ref() as *const T
+            ptr: self.data.as_ref() as *const T,
+            indicator
         }
     }
 }
