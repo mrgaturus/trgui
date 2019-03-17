1dc2a2e98d00a1178d14e9c10ea4e87a89a76d40
mrgaturus <mrgaturus@gmail.com>
2019-03-17 13:27:15 -0500

    Layout: change FixedLayout to EmptyLayout

--- src/decorator.rs
+++ src/decorator.rs
@@ -5,11 +5,6 @@ pub trait Decorator {
     fn before(&mut self, _: &WidgetInternal) {}
     fn after(&mut self, _: &WidgetInternal) {}
     fn update(&mut self, _: &WidgetInternal) {}
-
-    #[inline]
-    fn boxed(self) -> Box<Self> where Self: Sized {
-        Box::new(self)
-    }
 }
 
 pub struct EmptyDecorator;
--- src/layout.rs
+++ src/layout.rs
@@ -1,51 +1,12 @@
 use crate::widget::{WidgetInternal, Dimensions};
+use crate::Boxed;
 
 pub trait Layout {
-    fn layout(&self, widgets: &mut [WidgetInternal], dimensions: &Dimensions);
-    fn minimum_size(&self, widgets: &[WidgetInternal]) -> Dimensions;
+    fn layout(&self, _: &mut [WidgetInternal], _: &Dimensions) {}
+    fn minimum_size(&self, _: &[WidgetInternal]) -> Dimensions { (0, 0) }
 }
 
-pub struct FixedLayout { 
-    clamp: bool
-}
+pub struct EmptyLayout;
 
-impl FixedLayout {
-    pub fn new(clamp: bool) -> Self {
-        FixedLayout {
-            clamp
-        }
-    }
-}
-
-impl Layout for FixedLayout {
-    fn layout(&self, widgets: &mut [WidgetInternal], dimensions: &Dimensions) {
-        for widget in widgets {
-            let widget_bounds = widget.boundaries();
-            if self.clamp {
-                let mut new_pos = (widget_bounds.0, widget_bounds.1);
-                
-                new_pos.0 = if widget_bounds.0 < 0 {
-                    0
-                } else if widget_bounds.0 + widget_bounds.2 > dimensions.0 {
-                    dimensions.0 - widget_bounds.2
-                } else {
-                    widget_bounds.0
-                };
-
-                new_pos.1 = if widget_bounds.1 < 0 {
-                    0
-                } else if widget_bounds.1 + widget_bounds.3 > dimensions.1 {
-                    dimensions.1 - widget_bounds.3
-                } else {
-                    widget_bounds.1
-                };
-
-                widget.set_coordinates(new_pos);
-            }
-        }
-    }
-
-    fn minimum_size(&self, _: &[WidgetInternal]) -> Dimensions {
-        (0, 0)
-    }
-}
+impl Layout for EmptyLayout {}
+impl Boxed for EmptyLayout {}
