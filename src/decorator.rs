d8bf9e13215e5a5654a10c0f6f85f018b2339b4f
mrgaturus <mrgaturus@gmail.com>
2019-03-24 00:15:49 -0500

    Decorator & Layout: better trait declaration

--- src/decorator.rs
+++ src/decorator.rs
@@ -2,12 +2,16 @@ use crate::widget::WidgetInternal;
 use crate::Boxed;
 
 pub trait Decorator<P, D> {
-    fn before(&mut self, _: &WidgetInternal<P, D>) {}
-    fn after(&mut self, _: &WidgetInternal<P, D>) {}
-    fn update(&mut self, _: &WidgetInternal<P, D>) {}
+    fn before(&mut self, internal: &WidgetInternal<P, D>);
+    fn after(&mut self, internal: &WidgetInternal<P, D>);
+    fn update(&mut self, internal: &WidgetInternal<P, D>);
 }
 
 pub struct EmptyDecorator;
 
-impl<P, D> Decorator<P, D> for EmptyDecorator {}
+impl<P, D> Decorator<P, D> for EmptyDecorator {
+    fn before(&mut self, _: &WidgetInternal<P, D>) {}
+    fn after(&mut self, _: &WidgetInternal<P, D>) {}
+    fn update(&mut self, _: &WidgetInternal<P, D>) {}
+}
 impl Boxed for EmptyDecorator {}
--- src/layout.rs
+++ src/layout.rs
@@ -2,8 +2,8 @@ use crate::widget::{Dimensions, WidgetInternal};
 use crate::Boxed;
 
 pub trait Layout<P, D> {
-    fn layout(&self, _: &mut [WidgetInternal<P, D>], _: &Dimensions<D>) {}
-    fn minimum_size(&self, _: &[WidgetInternal<P, D>]) -> Dimensions<D>;
+    fn layout(&self, internal_list: &mut [WidgetInternal<P, D>], dim: &Dimensions<D>);
+    fn minimum_size(&self, internal_list: &[WidgetInternal<P, D>]) -> Dimensions<D>;
 }
 
 pub struct EmptyLayout;
@@ -12,6 +12,8 @@ impl<P, D> Layout<P, D> for EmptyLayout
 where
     D: Default,
 {
+    fn layout(&self, _: &mut [WidgetInternal<P, D>], _: &Dimensions<D>) {}
+
     fn minimum_size(&self, _: &[WidgetInternal<P, D>]) -> Dimensions<D> {
         (Default::default(), Default::default())
     }
