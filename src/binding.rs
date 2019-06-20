ece97ffa618295c917bb9ef9a69a2924a2c2545b
mrgaturus <mrgaturus@gmail.com>
2019-06-19 23:14:50 -0500

    Binding: remove closure stuff

--- src/binding.rs
+++ src/binding.rs
@@ -1,48 +1,36 @@
+// FIXME: figure how to use something like Pin
+
 //! Single-Threaded pointers that avoids the borrow checker for use inside widgets
 use crate::group::{push_event, GroupEvent::Signal, GroupID};
 
 /// Shares external data to (not limited to) widgets.
 ///
 /// A BindProxy is a Single-Threaded Pointer without borrow checker for the widgets.
+/// it only can store types without lifetime parameters, if you type has lifetime
+/// parameters, use unsafe opaque pointer instead.
 pub struct BindProxy<T> {
     ptr: *const T,
 }
 
 impl<T> BindProxy<T> {
-    /// Returns a non-mutable safe reference
+    /// Returns a non-mutable reference
     #[inline]
     pub fn read(&self) -> &T {
         unsafe { &*self.ptr }
     }
 
     #[inline]
-    /// Returns a raw pointer of the data
-    pub unsafe fn write_ptr(&self) -> *mut T {
-        self.ptr as *mut T
+    /// Returns a mutable reference
+    pub fn read_write(&self) -> &mut T {
+        unsafe { &mut *(self.ptr as *mut T) }
     }
 
-    /// Write on the data using a non-capturing closure with a mutable
-    /// reference as an argument
-    pub fn write<F>(&self, func: F)
-    where
-        F: Fn(&mut T),
-    {
-        unsafe {
-            func(&mut *(self.ptr as *mut T));
-        }
-    }
-
-    /// Write on the data using a non-capturing closure with a mutable
-    /// reference as an argument and push a signal to the event queue
-    pub fn write_push<F>(&self, func: F, id: GroupID)
-    where
-        F: Fn(&mut T),
-    {
-        unsafe {
-            func(&mut *(self.ptr as *mut T));
-        }
-
+    #[inline]
+    /// Returns a mutable reference and push a signal id into queue
+    pub fn rw_push(&self, id: GroupID) -> &mut T {
         push_event(Signal(id));
+
+        unsafe { &mut *(self.ptr as *mut T) }
     }
 }
 
